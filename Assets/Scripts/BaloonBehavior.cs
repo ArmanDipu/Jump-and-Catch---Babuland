808496dcb2e49b7eb65d60835f385d827f9bdf10
Arman Hossain Dipu Khan <80636334+ArmanDipu@users.noreply.github.com>
2023-07-24 05:25:56 +0600

    Balloon Concept Added

--- Assets/Scripts/BaloonBehavior.cs
+++ Assets/Scripts/BaloonBehavior.cs
@@ -7,25 +7,36 @@ using UnityEngine;
 public class WatermelonBehavior : MonoBehaviour
 {
     public Rigidbody2D balloonRigidbody2D;
-    private float deadZone = -5f;
+    private Collider2D catchCollider2D;
+    public float lifeTime = 5f;
+    private float timer = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
+        catchCollider2D = GameObject.FindGameObjectWithTag("hasCatchCollider2D").GetComponent<Collider2D>();
         balloonRigidbody2D.velocity = new Vector2(Random.Range(-1f, 1f), balloonRigidbody2D.velocity.y);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Balloon Caught");
-        Destroy(gameObject);
+        if (collision == catchCollider2D)
+        {
+            Debug.Log("Balloon Caught");
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y < deadZone)
+        if(timer > lifeTime)
         {
             Destroy(gameObject);
+        }
+        else
+        {
+            timer += Time.deltaTime;
         }
     }
 }
